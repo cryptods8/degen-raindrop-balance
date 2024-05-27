15a1c3c65011ad105417aed7946b0f9f2cd26d6b
cryptods8 <dusoo.s+ds8@gmail.com>
2024-05-27 16:03:50 +0200

    install button

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,28 @@ export async function generateMetadata(): Promise<Metadata> {
   };
 }
 
+function constructCastActionUrl(params: { url: string }): string {
+  // Construct the URL
+  const baseUrl = "https://warpcast.com/~/add-cast-action";
+  const urlParams = new URLSearchParams({
+    url: params.url,
+  });
+
+  return `${baseUrl}?${urlParams.toString()}`;
+}
+
 export default async function Home() {
+  const installUrl = constructCastActionUrl({
+    url: `${
+      process.env.NEXT_PUBLIC_HOST ?? "http://localhost:3000"
+    }/api/actions/balance`,
+  });
   return (
     <div
       style={{
         display: "flex",
+        flexDirection: "column",
+        gap: "1rem",
         height: "100dvh",
         width: "100vw",
         backgroundColor: "black",
@@ -29,6 +46,24 @@ export default async function Home() {
           rel="noopener noreferrer"
         >
           ds8
+        </a>
+      </div>
+      <div>
+        <a
+          style={{
+            display: "inline-block",
+            padding: "0.75rem 1.25rem",
+            border: "1px solid white",
+            borderRadius: "0.25rem",
+            textDecoration: "none",
+            color: "white",
+            fontWeight: 600,
+          }}
+          href={installUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Install
         </a>
       </div>
     </div>
